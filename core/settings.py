7f2551273765e3b5e5aa2a4d4fc60ff4cb5f1b80
Avinash <avinashkumar3013@gmail.com>
2022-05-07 16:54:10 +0530

    Added sentry config

--- core/settings.py
+++ core/settings.py
@@ -191,3 +191,20 @@ CHANNEL_LAYERS = {
 # }
 
 ACTIVE_ROOMS=[]
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_URL', None),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
